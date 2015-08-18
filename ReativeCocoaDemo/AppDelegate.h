beb6155a3cd6c395e35da1eb626ff6a445599feb
龚欣 <87526845@qq.com>
2015-08-19 07:36:52 +0800

    Initial Commit

---
+++ ReativeCocoaDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReativeCocoaDemo
+//
+//  Created by 龚欣 on 15/8/19.
+//  Copyright (c) 2015年 gong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ReativeCocoaDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ReativeCocoaDemo
+//
+//  Created by 龚欣 on 15/8/19.
+//  Copyright (c) 2015年 gong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
